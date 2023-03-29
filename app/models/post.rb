7251b15c8819315b9ddf233a8e0be357c4760dc0
Victor <victorgmassimi.microverse@gmail.com>
2023-03-29 16:03:51 -0600

    updated post to update counter on save

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id', dependent: :destroy
   belongs_to :author, class_name: 'User'
 
-  def update_post_counter
-    author.update(posts_counter: author.posts.count)
-  end
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
   end
 end
