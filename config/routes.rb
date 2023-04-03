a238c7de216c39fca6b69b385eeb30a064f74e79
Victor <victorgmassimi.microverse@gmail.com>
2023-04-03 10:45:52 -0600

    Fixed linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
