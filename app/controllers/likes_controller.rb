20703045d98f98e976e2b841706671b4392d6f1d
Victor <victorgmassimi.microverse@gmail.com>
2023-04-06 19:52:58 -0600

    fixed linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
     @like = Like.new(like_params)
     @like.author = current_user
     respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path)  } if @like.save
+      format.html { redirect_back(fallback_location: root_path) } if @like.save
     end
   end
 
