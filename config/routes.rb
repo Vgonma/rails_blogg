5c7d46b9c4d63b869758bbe6e879aed3b67103fb
Victor <victorgmassimi.microverse@gmail.com>
2023-04-06 19:51:35 -0600

    Add Likes counter

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users do
     resources :posts do
-      resources :comments
+      resources :comments, :likes
     end
   end
-
+  # post '/users/:user_id/posts/:post_id/likes', to likes#create
 end
