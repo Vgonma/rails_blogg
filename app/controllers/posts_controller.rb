8a4a0a4d998032b72de740cbd3ab9a2f10203201
belaymit <2belamit@gmail.com>
2023-04-12 20:31:57 +0300

    Added styles to registartion page and main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts_list.includes(:comments)
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+    @comments = @posts.comment.includes([:author])
   end
 
   def new
