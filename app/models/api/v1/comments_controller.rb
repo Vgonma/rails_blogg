29061d22fdda6a8091ca136349981d6ff68025fb
belaymit <2belamit@gmail.com>
2023-04-13 23:38:11 +0300

    Added create_comment method

--- app/models/api/v1/comments_controller.rb
+++ app/models/api/v1/comments_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::CommentsController < Api::V1::ApplicationController
   def index
     comments = Post.find(params[:post_id]).comments
-    render json: comments, status: ok
+    render json: comments, status: :ok
   end
   
   def create
+    comment = Comment.new(comment_params, post_id:, user_id:)
+    if comment.save
+      render json: comment, status: :created 
+    else
+      render json: {errors: comment.error}, status: :unprocessable_entity
+    end
+    
   end
 
-  prviate
-  def comment_params 
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
